03b05ebabeb4e953ede3a51c667f18af45d6822f
eduardo.yuschuk <eduardo.yuschuk@gmail.com>
2022-06-26 21:14:25 -0300

    -

--- main.go
+++ main.go
@@ -1,11 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
 
+	"github.com/go-redis/redis/v8"
+)
 
 func main() {
 
+	fmt.Println("Starting...")
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
 
-	fmt.Println(":P")
+	key := "demo1"
 
+	err := rdb.Set(ctx, key, "1", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	refTime := time.Now()
+	count := 0
+	for {
+		val, err := rdb.Get(ctx, key).Result()
+		if err != nil {
+			panic(err)
+		}
+		now := time.Now()
+		if now.Second() != refTime.Second() {
+			fmt.Println(time.Now(), key, val, count)
+			refTime = now
+			count = 0
+		}
+		count += 1
+	}
 }
